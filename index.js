933be19a3f61e77fff86f36e59920caace657b07
emoneemu <md.sazid.al.emon@gmail.com>
2023-03-17 11:10:10 +0600

    done with loader,searching and showing results

--- index.js
+++ index.js
@@ -15,10 +15,34 @@ document.querySelector('#search').addEventListener('submit',async (e)=>
 {
     e.preventDefault();
     const username = document.querySelector('#findByUsername').value;
-    console.log(username);
     const profile = await getUser(username);
+
+    if(username.length>0)
+    {
+      document.querySelector('.loader').style.display = 'block';
+      document.querySelector('.user-details').style.display = 'none';
+      document.querySelector('.notFound').style.display = 'none';
+      const profile = await getUser(username);
+      document.querySelector('.loader').style.display = 'none';
+
+
+      if(profile.message === 'Not Found')
+      {
+        document.querySelector('.notFound').style.display = 'block';
+      }
+      else{
+        const repos = await getRepos(profile);
+        document.querySelector('.user-details').style.display = '';
+        showProfile(profile);
+        showRepos(repos);
+      }
+    }
+    console.log(username);
+    
+    
     console.log(profile);
-    showProfile(profile);
+    document.querySelector('#findByUsername').value = '';
+   
 });
 
 function showProfile(profile)
@@ -53,10 +77,36 @@ function showProfile(profile)
     `;
 
 }
-showRepo()
+async function getRepos(profile)
 {
-    
+    const res = await fetch(`${profile.repos_url}?client_id=${client_id}&client_secret=${client_secret}&per_page=10`);
+    const repo = await res.json();
+    return repo;
 }
+
+function showRepos(repos)
+{
+    let newHtml = '';
+    for(let repo of repos)
+    {
+        newHtml+=`
+        <div class="repo">
+          <div class="repo_name">
+            <a href="${repo.html_url}">${repo.name}</a>
+          </div>
+          <p>
+            <span class="circle"></span> ${repo.language}
+            <ion-icon name="star-outline"></ion-icon> ${repo.watchers}
+            <ion-icon name="git-branch-outline"></ion-icon> ${repo.forks_count}
+          </p>
+        </div>
+        `;
+    }
+
+    document.querySelector('.repos').innerHTML = newHtml;
+}
+
+
 
 /*
 
